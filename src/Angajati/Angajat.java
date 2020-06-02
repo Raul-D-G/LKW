8e639a374970da6088add4137418fa0fec08ce10
Gioancă Raul <raul-dumitru.gioanca@my.fmi.unibuc.ro>
2020-06-02 18:56:09 +0300

    e1

--- src/Angajati/Angajat.java
+++ src/Angajati/Angajat.java
@@ -1,51 +1,26 @@
 package Angajati;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public abstract class Angajat {
 
-    private String functie;
-    private String nume;
-    private int vechime;
-    private boolean disponibil;
-    private static int numarAngajati = 0;
+    private final StringProperty id;
+    private final StringProperty functie;
+    private final StringProperty nume;
+    private final StringProperty vechime;
+    private final StringProperty disponibil;
 
-    public Angajat(String functie, String nume, int vechime, boolean disponibil) {
-        this.functie = functie;
-        this.nume = nume;
-        this.vechime = vechime;
-        this.disponibil = disponibil;
-        ++numarAngajati;
+    public Angajat(String id, String functie, String nume, String vechime, String disponibil) {
+        this.id = new SimpleStringProperty(id);
+        this.functie = new SimpleStringProperty(functie);
+        this.nume = new SimpleStringProperty(nume);
+        this.vechime = new SimpleStringProperty(vechime);
+        this.disponibil = new SimpleStringProperty(disponibil);
     }
 
-    protected void concediere() {
-        --numarAngajati;
-        System.out.println("Angajatul " + nume + " a fost concediat");
-    }
 
-    public static int nrAngajati() {
-        return numarAngajati;
-    }
 
-    public abstract int calculSalariu();
-
-    public String getNume() {
-        return nume;
-    }
-
-    public int getVechime() {
-        return vechime;
-    }
-
-    public String getFunctie() {
-        return functie;
-    }
-
-    public boolean isDisponibil() {
-        return disponibil;
-    }
-
-    public void setDisponibil(boolean disponibil) {
-        this.disponibil = disponibil;
-    }
 
     @Override
     public String toString() {
